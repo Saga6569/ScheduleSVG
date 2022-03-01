ba897e4af754db9b88b6dc83e2115c18d76738ea
Saga6569 <aristov6569@gmail.com>
2022-03-01 19:47:33 +0500

    text

--- src/components/VertillePlot/VertillePlot.tsx
+++ src/components/VertillePlot/VertillePlot.tsx
@@ -1,13 +1,46 @@
 import React, { useState } from 'react';
 import styles from './VertillePlot.module.css';
 import _ from 'lodash'
-import Pdf from "react-to-pdf";
+//import Pdf from "react-to-pdf";
 
-const PopUpWindow = (point: {x: number; y: number; color: string, name: string, value: number, visit: boolean}) => {
-  const textEnd = <text   x={point.x - 20 } y={point.y - 10} font-size="6" fill="black">{point.visit === false ? '' : `${point.name} ${point.value}`}</text>
-  const scr =  <rect width="40" className={styles.informationWndow} x={point.x - 20} 
-  y={point.y - 20} height="15" fill={point.color} stroke-width='1' opacity={point.visit === true ? 1 : 0} stroke="none" />
-  return (<svg >{scr}{textEnd}</svg>);
+
+interface Ipoint {
+  x: number; 
+  y: number; 
+  color: string, 
+  name: string, 
+  value: number, 
+  visit: boolean
+}
+// opacity={point.visit === true ? 1 : 0}
+
+const Square = (point: Ipoint) => {
+
+  const left = point.x === 0 ?  200 : point.x
+  const top = point.y === 0 ?  200 : point.y
+
+  const styleClass: {top: string, left: string} = {top: `${top - 235}px`, left: `${left - 560}px`};
+  return (<div style ={styleClass} className={styles.informationWndowT} > 
+      <svg 
+      width="50" height="50" 
+      preserveAspectRatio="xMidYMid meet">
+      <text x='0' y='25' font-size="5" opacity={point.visit === true ? 1 : 0} fill="black">{`${point.name} ${point.value}`}</text>
+     </svg>
+     </div>
+  )
+};
+
+
+const PopUpWindow = (point: Ipoint) => {
+  const scr =  <rect width="40" className={styles.informationWndow} x={point.x - 20} opacity={point.visit === true ? 1 : 0}
+  y={point.y - 20} height="15" fill={point.color} stroke-width='1'  stroke="none">
+  </rect>
+  
+  return (<>
+    <svg>
+      {scr}
+    </svg>
+  </>)
 };
 
 interface IGraphProps {
@@ -22,8 +55,7 @@ interface IGraphProps {
   }
 
 const VertillePlot = (props: IGraphProps) => {
-
-  const ref = React.createRef();
+  //const ref = React.createRef();
 
   const upDate = () => {
     const colors = ['blue', 'red', '#5aa5c4', 'tomato', 'green', 'MediumOrchid', 'Yellow', 'Lime', 'LightCyan'];
@@ -75,21 +107,22 @@ const VertillePlot = (props: IGraphProps) => {
          return result
       };
       
-      const long = props.values.length * 50 + 45 // Длинна горизонтальных линий
-      const v = 40 * 10 + 30 // Длинна горизонтальных линий
+      const lengthHorizontalLines = props.values.length * 50 + 45 // Длинна горизонтальных линий
+      const LengthVerticalLines = 40 * 10 + 30 // Длинна вериткальных линий
       
       const creatingVerticalGrid  = () => {
         let initPointX = 45;
         const result = [];
         for(let i = 0; i <= dataSort.length - 1; i++) {
-          result[i] =  <path d={`M${initPointX} ${v} V ${0}Z`} fill="transparent" stroke='#696666' stroke-width="1"/>
+          result[i] =  <path d={`M${initPointX} ${LengthVerticalLines} V ${0}Z`} fill="transparent" stroke='#696666' stroke-width="1"/>
           initPointX += 50
         }
        return result;
       };
 
+
       const creatingHorizontalGrid = () => {
-        const numberHorizontalLines = 10;
+        let numberHorizontalLines = 10;
         const result = [];
         const maxValueEl = dataSort[0].value;
 
@@ -99,31 +132,39 @@ const VertillePlot = (props: IGraphProps) => {
         const roundedAverage = '0'.repeat(String(roundedMaximumNumber).length - 1); // Получаем количество нулей для округления среднего значения значения
         const step  = Math.round(roundedMaximumNumber / numberHorizontalLines / Number(`1${roundedAverage}`) ) * Number(`1${roundedAverage}`) // округляем среднее значение
 
-       
-
         let initPointY = 0;
         let acc = 0;
-        for(let i = 0; i <= numberHorizontalLines; i++) {
+        for(let i = 0; maxValueEl >= acc ; i++) {
           const companent = <>
           <text x={15} y={428 - initPointY} font-size="10" fill="black" >{`${acc.toFixed()}`}</text> 
-          <path d={`M${long} ${430 - initPointY} H ${0}Z`} fill="transparent" stroke='#696666' stroke-width="1"/>
+          <path d={`M${lengthHorizontalLines} ${430 - initPointY} H ${0}Z`} fill="transparent" stroke='#696666' stroke-width="1"/>
           </>
           result[i] =  companent;
           initPointY += 40
           acc += step
         }
-       return result;
+      
+        const lastCompanent = <>
+        <text x={15} y={428 - initPointY} font-size="10" fill="black" >{`${acc.toFixed()}`}</text> 
+        <path d={`M${lengthHorizontalLines} ${430 - initPointY} H ${0}Z`} fill="transparent" stroke='#696666' stroke-width="1"/>
+        </>
+
+      result[result.length] = lastCompanent;
+      return result;
       };
 
-    const coponentrender = (
+    return (
       <div className={styles.container} >
-        <svg  width={long + 50} height="450" xmlns="http://www.w3.org/2000/svg" >
-        <rect x="0" y="0" width={long + 50} height="500" fill='#c0c0fa'/>
+       
+        <svg  width={lengthHorizontalLines + 50} height="450" xmlns="http://www.w3.org/2000/svg" >
+        <rect x="0" y="0" width={lengthHorizontalLines + 50} height="500" fill='#c0c0fa'/>
         {PopUpWindow(point)}
+       
         {creatingHorizontalGrid()}
         {creatingVerticalGrid()}
         {createDataForRendering()}
         </svg>
+        {Square(point)}
       </div>
     );
 
@@ -139,14 +180,14 @@ const VertillePlot = (props: IGraphProps) => {
     //   </div>
     // )
 
-    return (<>
-       <Pdf targetRef={ref} x={2} y={2} scale={1.1} filename="code-example.pdf">
-        {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
-      </Pdf>
-      <div ref={ref}>
-        {coponentrender}
-      </div>
-    </>)
+    // return (<>
+    //    <Pdf targetRef={ref} x={2} y={2} scale={1.1} filename="code-example.pdf">
+    //     {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+    //   </Pdf>
+    //   <div ref={ref}>
+    //     {coponentrender}
+    //   </div>
+    // </>)
 };
 
 export default VertillePlot;
